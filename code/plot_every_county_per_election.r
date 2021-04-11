f63d3b3bfe62d976380f2ae635d4ab913026d31a
yters <eric.holloway@gmail.com>
2021-04-11 16:25:42 -0400

    solid best fit line

--- code/plot_every_county_per_election.r
+++ code/plot_every_county_per_election.r
@@ -16,7 +16,7 @@ votes <- sum(reg_votes[reg_votes[,4]=='voted'&reg_votes[,3]==election,5:134])
 
 png(paste('images/_all_counties_',year,'.png',sep=''))
 matplot(t(reg_votes[reg_votes[,4]=='voted'&reg_votes[,3]==election,5:134]/reg_votes[reg_votes[,4]=='all_reg'&reg_votes[,3]=='2012-11-06',5:134]), type='l', ylab='Turnout ratio', xlab='Voter age in 2020', ylim=c(0,1))
-lines(35:95, fitted(fit)[1:61], col='black', pch=20, lwd=10, lty=6)
+lines(35:95, fitted(fit)[1:61], col='black', pch=20, lwd=10, lty=1)
 title(paste('All NC counties turnout ratios',year,'votes =',votes,'R =',round(r_value,3)))
 tmp <- dev.off()
 }
