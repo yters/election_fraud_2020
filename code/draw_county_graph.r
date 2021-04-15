8c85ac72e1894b40ccf4303db85744b3744a8c1a
yters <eric.holloway@gmail.com>
2021-04-15 14:25:16 -0400

    fixed code so high prediction accuracy regained

--- code/draw_county_graph.r
+++ code/draw_county_graph.r
@@ -1,18 +1,18 @@
 library(reshape2) # for the melt command
 
-fit_model <- function(votes, registrations, degrees) {
+fit_model <- function(votes, registrations, degrees, sample_count) {
     ratios <- votes/registrations
-    x <- rep(1:dim(votes)[2], 30)
-    y <- melt(t(ratios[sample(1:dim(votes)[1])[1:30],]))[,3]
-    y[!is.finite(y)] <- 0
+    x <- rep(1:dim(votes)[2], sample_count)
+    y <- melt(t(ratios[sample(1:dim(votes)[1],30),]))[,3]
+    #y[!is.finite(y)] <- 0
     fit <- lm(y ~ poly(x, degrees), na.action=na.exclude)
 }
 
 r_vals <- function(votes, registrations, fit) {
     r_vals <- c()
-    for(i in 1:98) {
+    for(i in 1:dim(votes)[1]) {
         truth <- unlist(votes[i,])
-        pred <- unlist(fitted(fit)[1:130]*registrations[i,])
+        pred <- unlist(fitted(fit)[1:dim(registrations)[2]]*registrations[i,])
         r_vals <- c(r_vals, cor.test(truth, pred)$estimate)
     }
     r_vals
@@ -21,12 +21,12 @@ r_vals <- function(votes, registrations, fit) {
 counties <- unlist(read.table('results/counties.txt', stringsAsFactors=FALSE))
 
 plot_graph <- function(year, votes, registrations, fit) {
-    for(i in 1:98) {
+    for(i in 1:dim(votes)[1]) {
         county_id <- i
         county <- counties[i]
         
         truth <- unlist(votes[i,])
-        pred <- unlist(fitted(fit)[1:130]*registrations[i,1:130])
+        pred <- unlist(fitted(fit)[1:130]*registrations[i,1:dim(registrations)[2]])
         r_val <- cor.test(truth, pred)$estimate
         
         png(paste('images/',year,'/',substr(format(round(r_val,3),nsmall=3),3,5),'-',county_id,'-',county,'-',year,'.png',sep=''))
@@ -47,9 +47,9 @@ votes_2020 <- read.table('results/votes_2020.txt')
 votes_2016 <- read.table('results/votes_2016.txt')
 votes_2012 <- read.table('results/votes_2012.txt')
 
-fit_2020 <- fit_model(votes_2020, registrations_2020, 6)
-fit_2016 <- fit_model(votes_2016, registrations_2016, 6)
-fit_2012 <- fit_model(votes_2012, registrations_2012, 6)
+fit_2020 <- fit_model(votes_2020, registrations_2020, 6, 30)
+fit_2016 <- fit_model(votes_2016, registrations_2016, 6, 30)
+fit_2012 <- fit_model(votes_2012, registrations_2012, 6, 30)
 
 plot_graph('2020', votes_2020, registrations_2020, fit_2020)
 plot_graph('2016', votes_2016, registrations_2016, fit_2016)
--- code/plot_every_county_per_election.r
+++ code/plot_every_county_per_election.r
@@ -9,14 +9,14 @@ plot_all_counties <- function(year, election) {
 voted <- reg_votes[reg_votes[,4]=='voted'&reg_votes[,3]==election,5:134]
 ratios <- voted/registered_2020
 x <- as.numeric(substr(as.character(melt(t(ratios[1:30,]))[,1]), 2,4))
-y <- melt(t(ratios[1:30,]))[,3]
-fit <- lm(y ~ poly(x, 10), na.action=na.exclude)
+y <- melt(t(ratios[sample(1:98,30),]))[,3]
+fit <- lm(y ~ poly(x, 6), na.action=na.exclude)
 # r_value <- sqrt(summary(fit)$adj.r.squared)
 crs <- c()
 for (i in 1:dim(ratios)[1]) {
 crs <- c(crs, cor.test(unlist(fitted(fit)[1:130]*registered_2020[i,]), unlist(voted[i,]))$estimate)
 }
-r_value <- median(crs)
+r_value <- mean(crs)
 
 votes <- sum(reg_votes[reg_votes[,4]=='voted'&reg_votes[,3]==election,5:134])
 
