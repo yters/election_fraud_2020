ec7d97b09e158dd268e91cf08c2aa8d2381d41f9
yters <eric.holloway@gmail.com>
2021-04-18 16:11:58 -0400

    remove moved directories from base

--- code/download_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-grep zip info/ncvhis_zip_urls.txt | sed 's/.*\(http.*zip\)".*$/\1/g' | xargs -I {} wget -P data/ {}
-grep zip info/ncvoter_zip_urls.txt | sed 's/.*\(http.*zip\)".*$/\1/g' | xargs -I {} wget -P data/ {}
